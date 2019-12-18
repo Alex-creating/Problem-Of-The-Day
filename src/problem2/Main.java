59500a617e6ac8ad9ae0522e3cc6dcba896fc33f
alex-creating <alex-russo1996@hotmail.co.uk>
2019-12-18 14:03:05 +0000

    2nd challenge complete

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -3,7 +3,7 @@ package problem2;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 101; i++) {
 			System.out.print(i + " ");
 
 			if (i % 3 == 0) {
